7192a4075c4bfa02eee9c4c9ce1c78f470fbf331
tom1299 <tom1299@jeeatwork.com>
2022-10-28 13:18:21 +0200

    Finish data exporter

--- data-exporter.js
+++ data-exporter.js
@@ -1,4 +1,8 @@
+const fs = require('fs')
+const os = require('os');
+const path = require('path');
 const { MongoClient } = require("mongodb");
+const { processJob } = require('./job-processor');
 
 const uri =
   "mongodb://localhost:27017?retryWrites=true&w=majority";
@@ -9,13 +13,14 @@ async function run() {
   try {
     const database = client.db('gitlab');
     const pipelines = database.collection('pipelines');
+    const dataDir = fs.mkdtempSync(path.join(os.tmpdir(), "data-"));
 
     const all_jobs = await pipelines.find({}, {"jobs": 1, _id: 0});
 
     await all_jobs.forEach((jobs) => {
       if (jobs.jobs !== undefined) {
         jobs.jobs.forEach((job) => {
-          console.debug(job.trace)
+          processJob(job, dataDir)
         });
       }
     });
--- job-processor.js
+++ job-processor.js
@@ -11,10 +11,12 @@ function getJobStatus(job_json) {
 }
 
 function processJob(job_json, root_folder) {
+    console.debug('Processing job ' + job_json.id)
     job_trace = extractTrace(job_json)
     if (job_trace == null)
         return false
     job_status = getJobStatus(job_json)
+    console.debug('Status of job ' + job_json.id + " is " + job_status)
     job_target_folder = root_folder + "/" + job_status
     if (!fs.existsSync(job_target_folder)){
         fs.mkdirSync(job_target_folder);
